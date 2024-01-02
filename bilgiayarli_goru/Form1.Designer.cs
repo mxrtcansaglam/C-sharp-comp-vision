cdd60d0889dff17b1a3a39100e472c902bcff5c9
mertcan sağlam <saglammertcan2@gmail.com>
2024-01-02 18:49:15 +0300

    Add project files.

---
+++ bilgiayarli_goru/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace bilgiayarli_goru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation2 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation3 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation4 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation5 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation6 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            lineAnnotation1.Name = "LineAnnotation1";
+            lineAnnotation2.Name = "LineAnnotation2";
+            lineAnnotation3.Name = "LineAnnotation3";
+            lineAnnotation4.Name = "LineAnnotation4";
+            lineAnnotation5.Name = "LineAnnotation5";
+            lineAnnotation6.Name = "LineAnnotation6";
+            this.chart1.Annotations.Add(lineAnnotation1);
+            this.chart1.Annotations.Add(lineAnnotation2);
+            this.chart1.Annotations.Add(lineAnnotation3);
+            this.chart1.Annotations.Add(lineAnnotation4);
+            this.chart1.Annotations.Add(lineAnnotation5);
+            this.chart1.Annotations.Add(lineAnnotation6);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(104, 278);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(527, 225);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(218, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(431, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 260);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(637, 278);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(297, 267);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "picurebox1";
+            this.columnHeader1.Width = 147;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "picurebox2";
+            this.columnHeader2.Width = 174;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 693);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ bilgiayarli_goru/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace bilgiayarli_goru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                
+                if (comboBox1.Text == "Histogram")
+                {
+                    DrawGrayScaleHistogram((Bitmap)pictureBox1.Image);
+                }
+                if (comboBox1.Text == "Gri yap")
+                {
+                    griformat((Bitmap)pictureBox1.Image);
+                }
+                pixelsayisi((Bitmap)pictureBox1.Image);
+                pixelsayisi((Bitmap)pictureBox2.Image);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen önce bir resim yükleyin!");
+            }
+        }
+        public void griformat(Bitmap resim)
+        {
+            Bitmap yeniResim = new Bitmap(resim.Width, resim.Height);
+
+            for (int x = 0; x < resim.Width; x++)
+            {
+                for (int y = 0; y < resim.Height; y++)
+                {
+                    Color pixelrenk = resim.GetPixel(x, y);
+                    int griDeger = (int)(pixelrenk.R * 0.3 + pixelrenk.G * 0.59 + pixelrenk.B * 0.11);
+                    Color yeniRenk = Color.FromArgb(griDeger, griDeger, griDeger);
+
+                    yeniResim.SetPixel(x, y, yeniRenk);
+                }
+            }
+
+            // Yeni resmi pictureBox2'ye at
+            pictureBox2.Image = yeniResim;
+        }
+        public void DrawGrayScaleHistogram(Bitmap resim)
+        {
+            
+            { // Histogram verilerini hesapla
+                int[] histogram = new int[256];
+
+                for (int x = 0; x < resim.Width; x++)
+                {
+                    for (int y = 0; y < resim.Height; y++)
+                    {
+                        Color pixelrenk = resim.GetPixel(x, y);
+                        int histogramdeger = (int)(pixelrenk.R * 0.3 + pixelrenk.G * 0.59 + pixelrenk.B * 0.11);
+                        histogram[histogramdeger]++;
+                    }
+                }
+
+                // Chart'ı temizle"
+                chart1.Series.Clear();
+
+                // Gri tonları için seriyi ekle
+                Series seriesGray = new Series("Gray");
+                seriesGray.ChartType = SeriesChartType.Column; // Grafik tipi belirle
+                seriesGray.Color = Color.Black;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    seriesGray.Points.AddXY(i, histogram[i]);
+                }
+
+                // Seriyi Chart'a ekle
+                chart1.Series.Add(seriesGray);
+
+                // Achsen stilini düzenle
+                chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Red;
+                chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Red;
+
+                // Bar genişliklerini düzenle
+                chart1.Series["Gray"].CustomProperties = "PointWidth = 0.5";
+
+                // Chart'ı güncelle
+                chart1.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        
+
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Histogram");
+            comboBox1.Items.Add("Gri yap");
+            comboBox1.Items.Add("KM intensity");
+            comboBox1.Items.Add("KM öklit RGB");
+            comboBox1.Items.Add("sobel");
+           
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void pixelsayisi(Bitmap resim)
+        {
+            if (resim == null)
+            {
+
+                listView1.Items.Add("pixsel değeri bulunmadı");
+            }
+            else
+            {
+                string deger1=pictureBox1.Image.ToString();
+                string deger2=pictureBox2.Image.ToString();
+                int pikselSayisi = resim.Width * resim.Height;
+                string pikselSayisiString = pikselSayisi.ToString();
+                ListViewItem item = new ListViewItem(deger1);
+                item.SubItems.Add(deger2);
+
+               
+                listView1.Items.Add(item);
+
+
+
+            }
+        }
+    }
+    }
+
+    
